e2d12a5d70893897e1aaff148142ca5943f4f249
echav <echav@EdgarsRoomComputer>
2022-06-30 10:24:39 -0700

    Added CallOutMenu method.  This method brings up the menu for the customer to select their order from.

--- EjsTacoShop/src/EjsTacoShopMain.java
+++ EjsTacoShop/src/EjsTacoShopMain.java
@@ -1,19 +1,27 @@
 
 import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 
 public class EjsTacoShopMain {
 
 	public static void main(String[] args) throws IOException{
 		PPPMenu();
+		CallOutMenu();
 	}
 
-	public static void PPPMenu() throws IOException{
-		FileOutputStream cafeMenu = new FileOutputStream ("EJsTacosMenu.txt");
-		PrintWriter menuItems = new PrintWriter(cafeMenu);
+	public static void PPPMenu() throws IOException{ 
+	/*This method will create the menu items, and allow for updates to the menu*/
 		
+		FileOutputStream cafeMenu = new FileOutputStream ("EJsTacosMenu.txt");   //Here FileOutputStream creates a new file "EJsTacosMenu.txt" if it doesn't already exist, and overwrites the file if it does exist
+		
+		PrintWriter menuItems = new PrintWriter(cafeMenu);                       //Here we will attach PrintWriter to the newly created text file, "EJsTacosMenu.txt".  PrintWriter allows us to write data to the file as simple strings
+		
+		/*menuItems has been declared as a PrintWriter object, and we use it here to add String to the text file "EJsTacosMenu.txt"*/
 		menuItems.println("Hello, here are todays menu Items\n");
 		menuItems.println("1.Tacos");
 		menuItems.println("\ta) Steak:    2.00");
@@ -37,5 +45,21 @@ public class EjsTacoShopMain {
 		
 		menuItems.close();
 	}
+	
+	public static void CallOutMenu() throws IOException {  
+	/*This method brings up the menu for the customer to select their order from*/
+		
+		FileInputStream readFile = new FileInputStream("EJsTacosMenu.txt");
+		Scanner outputMenu = new Scanner(readFile);
+		
+		while (outputMenu.hasNextLine()) {
+			String currentLine = outputMenu.nextLine();
+			System.out.println(currentLine);
+		}
+		
+		outputMenu.close();
+	}
+	
+	public static void
 	
 }
